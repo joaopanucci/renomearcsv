6b38050aa4560d821fecdeadbe10b93a54140c01
joaopanucci <joao.panucci@hotmail.com>
2025-08-28 15:14:14 -0400

    s

--- automacao1.py
+++ automacao1.py
@@ -33,7 +33,7 @@ DEBUGGER_ADDRESS = "127.0.0.1:9222"
 DEFAULT_TIMEOUT = 20
 
 """
-XPaths copiados diretamente
+XPaths copiados diretamente do site:
 
 """
 
@@ -47,19 +47,19 @@ XPATHS = {
 }
 
 MUNICIPIOS = [
-    "ÁGUA CLARA", "ALCINÓPOLIS", "AMAMBAI", "ANASTÁCIO", "ANAURILÂNDIA", "ANGÉLICA",
-    "ANTÔNIO JOÃO", "APARECIDA DO TABOADO", "AQUIDAUANA", "ARAL MOREIRA", "BANDEIRANTES",
-    "BATAGUASSU", "BATAYPORÃ", "BELA VISTA", "BODOQUENA", "BONITO", "BRASILÂNDIA", "CAARAPÓ",
-    "CAMAPUÃ", "CAMPO GRANDE", "CARACOL", "CASSILÂNDIA", "CHAPADÃO DO SUL", "CORGUINHO",
-    "CORONEL SAPUCAIA", "CORUMBÁ", "COSTA RICA", "COXIM", "DEODÁPOLIS", "DOIS IRMÃOS DO BURITI",
-    "DOURADINA", "DOURADOS", "ELDORADO", "FÁTIMA DO SUL", "FIGUEIRÃO", "GLÓRIA DE DOURADOS",
-    "GUIA LOPES DA LAGUNA", "IGUATEMI", "INOCÊNCIA", "ITAPORÃ", "ITAQUIRAÍ", "IVINHEMA", "JAPORÃ",
-    "JARAGUARI", "JARDIM", "JATEÍ", "JUTI", "LADÁRIO", "LAGUNA CARAPÃ", "MARACAJU", "MIRANDA",
-    "MUNDO NOVO", "NAVIRAÍ", "NIOAQUE", "NOVA ALVORADA DO SUL", "NOVA ANDRADINA",
-    "NOVO HORIZONTE DO SUL", "PARANAÍBA", "PARANHOS", "PEDRO GOMES", "PONTA PORÃ", "PORTO MURTINHO",
-    "RIBAS DO RIO PARDO", "RIO BRILHANTE", "RIO NEGRO", "RIO VERDE DE MATO GROSSO", "ROCHEDO",
-    "SANTA RITA DO PARDO", "SÃO GABRIEL DO OESTE", "SELVÍRIA", "SETE QUEDAS", "SIDROLÂNDIA",
-    "SONORA", "TACURU", "TAQUARUSSU", "TERENOS", "TRÊS LAGOAS", "VICENTINA",
+    "Água Clara","Alcinópolis","Amambai","Anastácio","Anaurilândia","Angélica",
+    "Antônio João","Aparecida do Taboado","Aquidauana","Aral Moreira","Bandeirantes",
+    "Bataguassu","Batayporã","Bela Vista","Bodoquena","Bonito","Brasilândia","Caarapó",
+    "Camapuã","Campo Grande","Caracol","Cassilândia","Chapadão do Sul","Corguinho",
+    "Coronel Sapucaia","Corumbá","Costa Rica","Coxim","Deodápolis","Dois Irmãos do Buriti",
+    "Douradina","Dourados","Eldorado","Fátima do Sul","Figueirão","Glória de Dourados",
+    "Guia Lopes da Laguna","Iguatemi","Inocência","Itaporã","Itaquiraí","Ivinhema","Japorã",
+    "Jaraguari","Jardim","Jateí","Juti","Ladário","Laguna Carapã","Maracaju","Miranda",
+    "Mundo Novo","Naviraí","Nioaque","Nova Alvorada do Sul","Nova Andradina",
+    "Novo Horizonte do Sul","Paraíso das Águas","Paranaíba","Paranhos","Pedro Gomes","Ponta Porã",
+    "Porto Murtinho","Ribas do Rio Pardo","Rio Brilhante","Rio Negro","Rio Verde de Mato Grosso",
+    "Rochedo","Santa Rita do Pardo","São Gabriel do Oeste","Selvíria","Sete Quedas","Sidrolândia",
+    "Sonora","Tacuru","Taquarussu","Terenos","Três Lagoas","Vicentina","Paraíso das Águas",
 ]
 
 
